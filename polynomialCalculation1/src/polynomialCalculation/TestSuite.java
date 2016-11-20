237710d9b56a1ec3bcfcff450c85a9189ab0f8b9
Eric <1486587187@qq.com>
2016-11-20 15:22:48 +0800

    lab5b黑盒测试提交

---
+++ polynomialCalculation1/src/polynomialCalculation/TestSuite.java
@@ -0,0 +1,13 @@
+package polynomialCalculation;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+   
+  @RunWith(Suite.class)
+  @Suite.SuiteClasses({
+    TestCaseDoCalculation.class
+    })
+  
+  public class TestSuite {
+    //the class remains empty,only used as a holder for above annotations.
+  }
